99a304d173ac3c2150298e43c902dfb0771da58e
emeraldhycient <igwezehycient86@gmail.com>
2022-05-12 21:05:07 +0100

    added footer

--- src/components/home/storeOverview.jsx
+++ src/components/home/storeOverview.jsx
@@ -9,7 +9,7 @@ function StoreOverview() {
         </h2>
       </div>
       <div className="md:w-5/12 md:mx-auto mx-4  mt-8">
-        <div className="grid grid-cols-4 gap-4 mb-4">
+        <div className="grid grid-cols-4 gap-4 mb-4 border-b-[.1rem] border-[#ffce1a]">
           <div className="bg-gray-200 h-fit  w-full flex justify-center items-center">
             <img src={shoe} loading="lazy" alt="" />
           </div>
@@ -20,11 +20,11 @@ function StoreOverview() {
             <p className="text-sm text-gray-400">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. .
             </p>
-            <a href="" className="my-4 text-sm">
+            <a href="" className="my-1 md:my-4 text-sm">
               View Details
             </a>
           </div>
-          <div className="w-full flex flex-col justify-evenly items-left">
+          <div className="w-full flex flex-col justify-between items-left">
             <div className="flex">
               <BsFillCartCheckFill size={25} className="text-[#ffce1a] mr-2" />
               <h4>234</h4>
@@ -33,7 +33,7 @@ function StoreOverview() {
           </div>
         </div>
 
-        <div className="grid grid-cols-4 gap-4 mb-4">
+        <div className="grid grid-cols-4 gap-4 mb-4 border-b-[.1rem] border-[#ffce1a]">
           <div className="bg-gray-200 h-fit  w-full flex justify-center items-center">
             <img src={shoe} loading="lazy" alt="" />
           </div>
@@ -44,11 +44,11 @@ function StoreOverview() {
             <p className="text-sm text-gray-400">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. .
             </p>
-            <a href="" className="my-4 text-sm">
+            <a href="" className="my-1 md:my-4 text-sm">
               View Details
             </a>
           </div>
-          <div className="w-full flex flex-col justify-evenly items-left">
+          <div className="w-full flex flex-col justify-between items-left">
             <div className="flex">
               <BsFillCartCheckFill size={25} className="text-[#ffce1a] mr-2" />
               <h4>234</h4>
@@ -57,7 +57,7 @@ function StoreOverview() {
           </div>
         </div>
 
-        <div className="grid grid-cols-4 gap-4 mb-4">
+        <div className="grid grid-cols-4 gap-4 mb-4 border-b-[.1rem] border-[#ffce1a]">
           <div className="bg-gray-200 h-fit  w-full flex justify-center items-center">
             <img src={shoe} loading="lazy" alt="" />
           </div>
@@ -68,11 +68,11 @@ function StoreOverview() {
             <p className="text-sm text-gray-400">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. .
             </p>
-            <a href="" className="my-4 text-sm">
+            <a href="" className="my-1 md:my-4 text-sm">
               View Details
             </a>
           </div>
-          <div className="w-full flex flex-col justify-evenly items-left">
+          <div className="w-full flex flex-col justify-between items-left">
             <div className="flex">
               <BsFillCartCheckFill size={25} className="text-[#ffce1a] mr-2" />
               <h4>234</h4>
@@ -81,7 +81,7 @@ function StoreOverview() {
           </div>
         </div>
 
-        <div className="grid grid-cols-4 gap-4 mb-4">
+        <div className="grid grid-cols-4 gap-4 mb-4 border-b-[.1rem] border-[#ffce1a]">
           <div className="bg-gray-200 h-fit  w-full flex justify-center items-center">
             <img src={shoe} loading="lazy" alt="" />
           </div>
@@ -92,11 +92,11 @@ function StoreOverview() {
             <p className="text-sm text-gray-400">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. .
             </p>
-            <a href="" className="my-4 text-sm">
+            <a href="" className="my-1 md:my-4 text-sm">
               View Details
             </a>
           </div>
-          <div className="w-full flex flex-col justify-evenly items-left">
+          <div className="w-full flex flex-col justify-between items-left">
             <div className="flex">
               <BsFillCartCheckFill size={25} className="text-[#ffce1a] mr-2" />
               <h4>234</h4>
