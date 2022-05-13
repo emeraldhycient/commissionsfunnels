166826c9ee721c390b98664d5e50edfd28783d38
emeraldhycient <igwezehycient86@gmail.com>
2022-05-13 16:32:46 +0100

    [update] updated layout login/signup

--- src/components/login/layout.jsx
+++ src/components/login/layout.jsx
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <section className="w-full h-fit md:h-screen py-5 md:py-0">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="flex flex-col justify-center items-center  text-left">
+        <div className="flex flex-col justify-center items-center  text-left mt-40 md:mt-0">
           {children}
         </div>
         <div className="bg-slate-900 h-screen hidden md:block">
