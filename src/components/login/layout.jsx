30ff78375b7b8d454cf2853101e62312290d6d3d
emeraldhycient <igwezehycient86@gmail.com>
2022-05-13 16:28:33 +0100

    [update] updated the login,signup layout holder for responsive screen

--- src/components/login/layout.jsx
+++ src/components/login/layout.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Layout({ children }) {
   return (
-    <section className="w-full h-screen">
+    <section className="w-full h-fit md:h-screen py-5 md:py-0">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="flex flex-col justify-center items-center  text-left">
           {children}
         </div>
-        <div className="bg-slate-900 h-screen">
+        <div className="bg-slate-900 h-screen hidden md:block">
           <img
             src="https://picsum.photos/300/300"
             className="h-screen w-full"
