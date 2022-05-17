1f11140d93658a6e3115bb61af882b57bb58675b
emeraldhycient <igwezehycient86@gmail.com>
2022-05-17 16:55:00 +0100

    [updated] sidebar

--- src/components/merchants/sidebar.jsx
+++ src/components/merchants/sidebar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 function Sidebar() {
   return (
     <div className="h-full flex flex-col justify-between">
-      <ul>
+      <ul className="mx-auto">
         <li className="text-gray-300 text-sm my-4 hover:text-yellow-500">
           <Link to="/" className="flex items-center ">
             <svg
-              className="h-6 w-6 mr-3"
+              className="h-5 w-5 mr-3"
               fill="none"
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -84,17 +84,15 @@ function Sidebar() {
         <li className="text-gray-300 text-sm my-4 hover:text-yellow-500">
           <Link to="/" className="flex items-center ">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5 mr-3"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-              <path
-                fillRule="evenodd"
-                d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                clipRule="evenodd"
-              />
+              <path d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
             <h6>Dispatchers</h6>
           </Link>
