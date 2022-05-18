e0b23410fe0f31279c3b2552dc1a29bedab4c5a6
emeraldhycient <igwezehycient86@gmail.com>
2022-05-18 23:28:23 +0100

    [update] added nice cool rgb random color to stats border top

--- src/components/merchants/vendor/stat.jsx
+++ src/components/merchants/vendor/stat.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 
 function Stat({ icon, name, total }) {
+  const colors = [
+    "border-t-[#f7d9d9]",
+    "border-t-[#bbf7d0]",
+    "border-t-[#d9f99d]",
+    "border-t-[#83cc16]",
+    "border-t-[#ffce1a]",
+    "border-t-[#fdbb74]",
+    "border-t-[#f7d9f9]",
+  ];
+
+  const random = Math.floor(Math.random() * colors.length);
+
+  const color = colors[random];
+
   return (
-    <div className=" mb-4  border-t-[.1rem] border-t-green-200 shadow-lg  h-16 shadow-lg flex">
+    <div
+      className={`mb-4  border-t-[.2rem] ${color} rounded-l-lg shadow-lg  h-16 shadow-lg flex`}
+    >
       <div className="w-[30%] bg-slate-900 h-16 rounded-r-lg rounded-l-lg flex justify-center items-center">
         {icon}
       </div>
