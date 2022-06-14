40dd982ea3054ff0d5c496ab14997819e8d5496d
emeraldhycient <igwezehycient86@gmail.com>
2022-06-14 19:19:46 +0100

    [updated] fupdated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,85 +1,41 @@
-import funnel from "../assets/images/sales-funnel.png";
+import React from "react";
 import { Link } from "react-router-dom";
-import { BsFacebook, BsTwitter } from "react-icons/bs";
-import { FaWhatsapp } from "react-icons/fa";
+import { MdMarkEmailUnread } from "react-icons/md";
+import { ImWhatsapp } from "react-icons/im";
+import { BsTwitter, BsLinkedin } from "react-icons/bs";
+import funnel from "../assets/images/sales-funnel.png";
 
 function Footer() {
+  const year = new Date().getFullYear();
+
   return (
-    <section className="w-full bg-slate-900 h-fit pt-4  mt-6">
-      <div className="mx-4 md:w-10/12 md:mx-auto mt-8 mb-8">
-        <div className="grid grid-cols-1 md:grid-cols-3">
-          <div className="mb-6">
-            <div className="flex items-center flex-shrink-0  mr-6">
-              <h2 className="text-xl text-slate-900 font-horta font-bold flex text-white items-center">
-                <span className="bg-[#ffce1a] text-white px-1">
-                  Commissions
-                </span>
-                Funnels <img loading="lazy" src={funnel} className="h-6 w-6" />
-              </h2>
-            </div>
-            <p className="text-gray-200 mt-4 md:mt-8">
-              lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-              ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-            <ul className="flex items-center mt-6 mb-8 md:mb-1">
-              <li className="mr-3">
-                <a href="http://">
-                  <BsFacebook size={25} className="text-white" />
-                </a>
-              </li>
-              <li className="mr-3">
-                <a href="http://">
-                  <FaWhatsapp size={25} className="text-white" />
-                </a>
-              </li>
-              <li className="mr-3">
-                <a href="http://">
-                  <BsTwitter size={25} className="text-white" />
-                </a>
-              </li>
-            </ul>
+    <section className="h-fit md:h-16 py-8 md:py-0 bg-gray-100  border-t border-slate-900">
+      <div className="grid grid-cols-1 md:grid-cols-2 h-full gap-6">
+        <div className="h-full flex justify-center items-center">
+          <div className="bg-slate-900  h-8 w-8 mx-3 flex justify-center items-center">
+            <a href="mailto:gofitish@gmail.com">
+              <MdMarkEmailUnread size={22} className="text-amber-500" />
+            </a>
           </div>
-          <div className="col-span-2 grid grid-cols-2 md:grid-cols-2">
-            <div className="mb-4 text-white text-left md:text-center">
-              <h4 className="text-lg font-semibold">Useful links</h4>
-              <ul className="mt-4 md:mt-8">
-                <li className="mt-2">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="mt-2">
-                  <Link to="/about">About</Link>
-                </li>
-                <li className="mt-2">
-                  <Link to="/contact">Contact</Link>
-                </li>
-                <li className="mt-2">
-                  <Link to="/login">Login</Link>
-                </li>
-              </ul>
-            </div>
-            <div className="mb-4 text-white text-center">
-              <h4 className="text-lg font-semibold">Information</h4>
-              <ul className="mt-4 md:mt-8">
-                <li className="mt-2">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="mt-2">
-                  <Link to="/about">About</Link>
-                </li>
-                <li className="mt-2">
-                  <Link to="/contact">Contact</Link>
-                </li>
-                <li className="mt-2">
-                  <Link to="/login">Login</Link>
-                </li>
-              </ul>
-            </div>
+          <div className="bg-slate-900  h-8 w-8 mx-3 flex justify-center items-center">
+            <a href="https://wa.me/2347088639675">
+              <ImWhatsapp className="text-amber-500" />
+            </a>
+          </div>
+          <div className="bg-slate-900  h-8 w-8 mx-3 flex justify-center items-center">
+            <BsTwitter className="text-amber-500" />
+          </div>
+          <div className="bg-slate-900  h-8 w-8 mx-3 flex justify-center items-center">
+            <BsLinkedin className="text-amber-500" />
           </div>
         </div>
-      </div>
-      <div className="h-12 bg-slate-500">
-        <div className="flex items-center justify-center text-white text-center font-medium pt-3">
-          <p className="text-sm">© 2022 Commissionsfunnels.</p>
+
+        <div className="h-full  flex  justify-center items-center">
+          ©️ {year}
+          <h1 className="font-extrabold text-lg text-slate-900 font-horta  flex ml-3">
+            <span className="text-white bg-[#ffce1a] px-1">commissions</span>
+            Funnels <img loading="lazy" src={funnel} className="h-6 w-6" />
+          </h1>
         </div>
       </div>
     </section>
